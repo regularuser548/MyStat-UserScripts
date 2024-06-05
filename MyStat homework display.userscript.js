31f97d8b11f8253173c4a94dc8a147344b04a6ac
regularuser548 <109815382+regularuser548@users.noreply.github.com>
2024-06-05 23:27:40 +0300

    Update 1.1

--- MyStat homework display.userscript.js
+++ MyStat homework display.userscript.js
@@ -3,9 +3,10 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://mystat.itstep.org/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      regularuser548
-// @description 4/27/2024, 5:12:31 AM
+// @description Скрипт который отображает текстовые дз прямо в Майстате
+// @license     MIT
 // ==/UserScript==
 
 const modalHTML = '<button class="float-right" id="modalCloseBtn">X</button> <p></p>';
@@ -14,74 +15,69 @@ const modalHTML = '<button class="float-right" id="modalCloseBtn">X</button> <p>
 document.addEventListener('click', clickHandler);
 
 function clickHandler(event) {
-  if (event.target.parentElement.parentElement.classList.contains("load-file"))
-    displayFile(event);
+    if (event.target.parentElement.parentElement.classList.contains("load-file"))
+        displayFile(event);
 }
 
 function closeDialog() {
-  document.querySelector('.customModal').close();
+    document.querySelector('.customModal').close();
 }
 
 function displayFile(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  let link = event.target.parentElement;
-  downloadFile(link);
+    let link = event.target.parentElement;
+
+    fetch(link, { method: 'HEAD' })
+        .then((response) => {
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            let contentType = response.headers.get("Content-Type").split(";")[0];
+
+
+            if (contentType == 'text/plain')
+                downloadFile(link);
+            else
+                link.click();
+
+        })
+
 }
 
 function downloadFile(link) {
-  fetch(link)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
+    fetch(link)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
 
-    if (response.headers.get("Content-Type").split(";")[0] == 'text/plain')
-      return response.text();
-    else
-      return response.blob();
-  })
-  .then((response) => {
+            return response.text();
 
-    if (typeof response === "string")
-      displayModal(response.trim());
-    else
-      downloadBinaryFile(response);
-  });
-}
+        })
+        .then((content) => {
+            displayModal(content.trim());
+        });
 
-function downloadBinaryFile(file) {
-    let a = document.querySelector('#customLink');
-
-    if (a == null) {
-         a = document.createElement("a");
-         a.id = 'customLink';
-         a.style = "display: none";
-         document.body.appendChild(a);
-      }
-
-    let url = window.URL.createObjectURL(file);
-    a.href = url;
-    a.download = file.fileName;
-    a.click();
-    window.URL.revokeObjectURL(url);
 }
 
 function displayModal(text) {
-  let container = document.querySelector('.homeworks-section');
-  let modal = container.querySelector('.customModal');
+    let container = document.querySelector('.homeworks-section');
+    let modal = container.querySelector('.customModal');
 
 
-  if (modal == null) {
-    //create dialog node
-    modal = document.createElement('dialog');
-    modal.innerHTML = modalHTML;
-    modal.classList.add("customModal");
-    modal.querySelector('#modalCloseBtn').onclick = closeDialog;
-    container.appendChild(modal);
-  }
+    if (modal == null) {
+        //create dialog node
+        modal = document.createElement('dialog');
+        modal.innerHTML = modalHTML;
+        modal.classList.add("customModal");
+        modal.querySelector('#modalCloseBtn').onclick = closeDialog;
+        container.appendChild(modal);
+    }
 
-  //update text
-  modal.querySelector('p').innerText = text;
-  modal.showModal();
+    //update text
+    modal.querySelector('p').innerText = text;
+    modal.showModal();
 }
