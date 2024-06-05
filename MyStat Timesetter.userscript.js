e99fab982329ba8a55cc7fd65c4dff1354eae5ec
regularuser548 <109815382+regularuser548@users.noreply.github.com>
2024-06-05 23:46:03 +0300

    MyStat Timesetter Update 1.0.1

--- MyStat Timesetter.userscript.js
+++ MyStat Timesetter.userscript.js
@@ -3,9 +3,10 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://mystat.itstep.org/*
 // @grant       none
-// @version     1.0
+// @version     1.0.1
 // @author      regularuser548
-// @description 4/26/2024, 11:14:19 PM
+// @description Скрипт который автоматически ставит время в окне загрузки дз в Майстате
+// @license     MIT
 // ==/UserScript==
 
 
@@ -15,18 +16,18 @@ const mins = '00';
 document.addEventListener('click', checkForDialog);
 
 function checkForDialog(event) {
-  if (event.target.parentElement.classList.contains("upload-file") || event.target.classList.contains("upload-file"))
-      if (document.getElementsByClassName('text-homework-time-spent-wrap'))
-        setDefaultTime();
+    if (event.target.parentElement.classList.contains("upload-file") || event.target.classList.contains("upload-file"))
+        if (document.getElementsByClassName('text-homework-time-spent-wrap'))
+            setDefaultTime();
 }
 
-function setDefaultTime(){
-  let timeInputs = document.getElementsByClassName('text-homework-time-spent-wrap')[0].querySelectorAll('input[type="text"]');
-  timeInputs[0].value = hours;
-  timeInputs[1].value = mins;
+function setDefaultTime() {
+    let timeInputs = document.getElementsByClassName('text-homework-time-spent-wrap')[0].querySelectorAll('input[type="text"]');
+    timeInputs[0].value = hours;
+    timeInputs[1].value = mins;
 
-  timeInputs[0].focus();
-  timeInputs[1].focus();
-  timeInputs[1].blur();
+    timeInputs[0].focus();
+    timeInputs[1].focus();
+    timeInputs[1].blur();
 
 }
